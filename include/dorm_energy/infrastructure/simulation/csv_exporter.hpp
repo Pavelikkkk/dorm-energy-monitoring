ac41ab63ad92473c3c95bed8a5177e737642cdda
Pavelikkkk <xxxleberto@mail.ru>
2026-05-31 21:59:04 +0300

    feat: add csv export generated data

---
+++ include/dorm_energy/infrastructure/simulation/csv_exporter.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "dorm_energy/core/measurement.hpp"
+
+#include <filesystem>
+
+namespace dorm_energy::simulation
+{
+
+    class CsvExporter
+    {
+    public:
+        static bool exportReadings(
+            const core::ReadingsBatch &readings,
+            const std::filesystem::path &filePath);
+    };
+
+}
---
+++ src/dorm_energy/infrastructure/simulation/csv_exporter.cpp
@@ -0,0 +1,59 @@
+#include "dorm_energy/infrastructure/simulation/csv_exporter.hpp"
+
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+
+namespace dorm_energy::simulation
+{
+
+    bool CsvExporter::exportReadings(
+        const core::ReadingsBatch &readings,
+        const std::filesystem::path &filePath)
+    {
+        std::filesystem::create_directories(
+            filePath.parent_path());
+
+        std::ofstream file(filePath);
+
+        if (!file.is_open())
+            return false;
+
+        file << "timestamp,device_id,sensor_type,value\n";
+
+        for (const auto &reading : readings)
+        {
+            double value = reading.value;
+
+            if (reading.sensorType == "motion")
+            {
+                value = reading.boolValue.value_or(false);
+            }
+
+            auto tt =
+                std::chrono::system_clock::to_time_t(
+                    reading.timestamp);
+
+            std::tm tm{};
+
+#ifdef _WIN32
+            localtime_s(&tm, &tt);
+#else
+            localtime_r(&tt, &tm);
+#endif
+
+            file
+                << std::put_time(&tm, "%Y-%m-%d %H:%M:%S")
+                << ","
+                << reading.deviceId
+                << ","
+                << reading.sensorType
+                << ","
+                << value
+                << "\n";
+        }
+
+        return true;
+    }
+
+}
